4f584f4e3e96a73df0b546ac621bc98586da2478
salmanul.haris <salmanul.haris@acodez.co.in>
2025-05-20 17:16:14 +0530

    Added pot file

--- tbs-webpressor.php
+++ tbs-webpressor.php
@@ -22,16 +22,7 @@ if (!defined('ABSPATH')) {
 define('TBSW_VERSION', '1.0.0');
 define('TBSW_PLUGIN_DIR', trailingslashit(dirname(__FILE__)));
 define('TBSW_PLUGIN_URL', trailingslashit(plugins_url('', __FILE__)));
-
-// Create languages directory if it doesn't exist
-if (!file_exists(TBSW_PLUGIN_DIR . 'languages')) {
-    global $wp_filesystem;
-    if (!function_exists('WP_Filesystem')) {
-        require_once(ABSPATH . 'wp-admin/includes/file.php');
-        WP_Filesystem();
-    }
-    $wp_filesystem->mkdir(TBSW_PLUGIN_DIR . 'languages', FS_CHMOD_DIR);
-}
+define('TBSW_PLUGIN_BASENAME', plugin_basename(__FILE__));
 
 /**
  * Include required files
